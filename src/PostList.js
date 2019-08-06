8066af05602d2c33073b181d90b75f3fe1679533
fangengen <330491693@qq.com>
2019-08-06 12:05:29 +0800

    Commit Message

---
+++ src/PostList.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import './PostList.css';
+import PostItem from './PostItem'
+
+
+class PostList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        };
+
+        this.timer = null;
+        this.handleVote = this.handleVote.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+    }
+
+    handleVote(id) {
+        const posts = this.state.posts.map(item => {
+            const newItem = item.id === id ? {...item, vote: ++item.vote} : item;
+            return newItem;
+        })
+
+        this.setState({
+            posts,
+        })
+    }
+
+    handleSave(post) {
+        const posts = this.state.posts.map(item => {
+            const newItem = item.id === post.id ? post : item;
+            return newItem;
+        })
+
+        this.setState({
+            posts,
+        })
+    }
+
+    componentWillUnmount() {
+        if (this.timer) {
+            clearTimeout(this.timer);
+        }
+    }
+
+    componentDidMount() {
+        this.timer = setTimeout(() => {
+            this.setState({
+                posts: [
+                    {id: 1, title: "大家一起来讨论React吧", author: "张三", date: "2017-09-01 10:00", vote: 0},
+                    {id: 2, title: "前端框架，你最爱哪一个", author: "李四", date: "2017-09-01 12:00", vote: 0},
+                    {id: 3, title: "Web App的时代已经到来", author: "王五", date: "2017-09-01 14:00", vote: 0},
+                ]
+            });
+        }, 5000)
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h2>帖子列表</h2>
+                <ul>
+                    {this.state.posts.map(item => <PostItem key={item.id} post={item} onVote={this.handleVote}
+                                                            onSave={this.handleSave}/>)}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default PostList;
+
+
+
+
+
